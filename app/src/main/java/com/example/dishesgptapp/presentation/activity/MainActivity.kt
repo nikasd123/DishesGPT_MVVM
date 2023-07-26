c8356d57c68d0a3a5e04579ad82af7df30c7029d
nikit <nikita_eremeev.95@mail.ru>
2023-07-26 15:22:13 +0300

    base navigation

--- app/src/main/java/com/example/dishesgptapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/dishesgptapp/presentation/activity/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.dishesgptapp.presentation.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.dishesgptapp.R
+import com.example.dishesgptapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
