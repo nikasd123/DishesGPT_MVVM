19f6ef239a6aad0d95c97b287861dd0c46c6031e
nikit <nikita_eremeev.95@mail.ru>
2023-08-09 22:30:38 +0300

    uninstallation fun-mapper

--- app/src/main/java/com/example/dishesgptapp/data/remote/dto/ChatGPTRequest.kt
+++ app/src/main/java/com/example/dishesgptapp/data/remote/dto/ChatGPTRequest.kt
@@ -1,6 +1,5 @@
 package com.example.dishesgptapp.data.remote.dto
 
-import com.example.dishesgptapp.domain.models.DomainGPTRecipesRequest
 import com.google.gson.annotations.SerializedName
 
 data class ChatGPTRequest(
@@ -16,11 +15,3 @@ data class ChatGPTRequest(
     @SerializedName("max_tokens")
     val maxTokens: Int?,
 )
-
-internal fun ChatGPTRequest.toDomainGPTRecipesRequest(): DomainGPTRecipesRequest =
-    DomainGPTRecipesRequest(
-        prompt = prompt,
-        model = model,
-        temperature = temperature,
-        maxTokens = maxTokens
-    )
