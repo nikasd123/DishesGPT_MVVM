93afdbff8ef5b7b86dea5841f072d10bd5b5d155
nikit <nikita_eremeev.95@mail.ru>
2023-08-09 22:39:33 +0300

    add use_case HILT provide

--- app/src/main/java/com/example/dishesgptapp/di/module/UseCasesModule.kt
+++ app/src/main/java/com/example/dishesgptapp/di/module/UseCasesModule.kt
@@ -1,4 +1,17 @@
 package com.example.dishesgptapp.di.module
 
+import com.example.dishesgptapp.domain.repository.GptRecipeRepository
+import com.example.dishesgptapp.domain.use_cases.GetGptRecipesUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 class UseCasesModule {
+
+    @Provides
+    fun provideGptRecipesUseCase(gptRecipeRepository: GptRecipeRepository) =
+        GetGptRecipesUseCase(gptRecipeRepository = gptRecipeRepository)
 }
