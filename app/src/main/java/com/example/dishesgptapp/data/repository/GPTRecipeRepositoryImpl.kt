8b9226e5bf66672c95a0f3cbeffa41546c116057
nikit <nikita_eremeev.95@mail.ru>
2023-08-17 23:40:45 +0300

    reworked domain logic

--- app/src/main/java/com/example/dishesgptapp/data/repository/GPTRecipeRepositoryImpl.kt
+++ app/src/main/java/com/example/dishesgptapp/data/repository/GPTRecipeRepositoryImpl.kt
@@ -4,7 +4,9 @@ import com.example.dishesgptapp.data.remote.dto.GPTRecipeChoice
 import com.example.dishesgptapp.data.repository.remote.GPTRecipesRemote
 import com.example.dishesgptapp.domain.repository.GptRecipeRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class GPTRecipeRepositoryImpl @Inject constructor(
     private val gptRecipesRemote: GPTRecipesRemote
 ): GptRecipeRepository{
--- app/src/main/java/com/example/dishesgptapp/domain/use_cases/GetGptRecipesUseCase.kt
+++ app/src/main/java/com/example/dishesgptapp/domain/use_cases/GetGptRecipesUseCase.kt
@@ -4,12 +4,14 @@ import com.example.dishesgptapp.data.remote.dto.toDomainGPTRecipeChoice
 import com.example.dishesgptapp.domain.models.DomainGPTRecipeChoice
 import com.example.dishesgptapp.domain.repository.GptRecipeRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class GetGptRecipesUseCase @Inject constructor(
     private val gptRecipeRepository: GptRecipeRepository
 ) {
     suspend fun getRecipes(prompt: String): List<DomainGPTRecipeChoice>? =
-        gptRecipeRepository.getGPTRecipes(prompt)?.map {
-            it.toDomainGPTRecipeChoice()
+        gptRecipeRepository.getGPTRecipes(prompt)?.map {gptRecipeChoice ->
+            gptRecipeChoice.toDomainGPTRecipeChoice()
         }
 }
